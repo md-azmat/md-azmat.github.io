b5d7035ce18aaaf1bbaec2c64c1ada324875d219
Md. Azmat Ansari <73055811+md-azmat@users.noreply.github.com>
2021-09-08 23:35:15 +0530

    Add files via upload

---
+++ updatedatabase.php
@@ -0,0 +1,16 @@
+<?php
+
+    session_start();
+
+    if (array_key_exists("content", $_POST)) {
+        
+        include("connection.php");
+        
+        $query = "UPDATE user SET diary = '".mysqli_real_escape_string($link, $_POST['content'])."' 
+        WHERE id = ".mysqli_real_escape_string($link, $_SESSION['id'])." LIMIT 1";
+        
+        mysqli_query($link, $query);
+        
+    }
+
+?>
